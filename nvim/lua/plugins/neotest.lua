42015ae11756f255b49c533b0409da6ed901c1ba
Dirli-V <github@dirli.net>
2023-02-23 20:39:24 +0100

    Add neotest keys

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -7,8 +7,14 @@ return {
   -- stylua: ignore
   keys = {
     { "<leader>dd", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run tests in file" },
-    { "<leader>dj", function() require("neotest").run.run({strategy = "dap"}) end, desc = "Run nearest test" },
+    { "<leader>dD", function() require("neotest").run.run({ vim.fn.expand("%"), strategy = "dap" }) end, desc = "Debug tests in file" },
+    { "<leader>dj", function() require("neotest").run.run() end, desc = "Run nearest test" },
+    { "<leader>dJ", function() require("neotest").run.run({ strategy = "dap" }) end, desc = "Debug nearest test" },
     { "<leader>do", function() require("neotest").output.open({ enter = true }) end, desc = "Open test output" },
+    { "<leader>ds", function() require("neotest").run.stop() end, desc = "Stop nearest test" },
+    { "<leader>dl", function() require("neotest").summary.toggle() end, desc = "List tests" },
+    { "<leader>dn", function() require("neotest").jump.next() end, desc = "Jump to next test" },
+    { "<leader>dN", function() require("neotest").jump.prev() end, desc = "Jump to prev test" },
   },
   opts = {},
   config = function(_, _)
