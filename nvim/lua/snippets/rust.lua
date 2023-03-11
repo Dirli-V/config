b4fdb19bbce094d05bb28a8748e0d8668ebe2460
Dirli-V <github@dirli.net>
2023-03-11 16:55:45 +0100

    Add rust snippets

--- nvim/lua/snippets/rust.lua
+++ nvim/lua/snippets/rust.lua
@@ -8,7 +8,36 @@ local f = ls.function_node
 local c = ls.choice_node
 local d = ls.dynamic_node
 local r = ls.restore_node
+local fmt = require("luasnip.extras.fmt").fmt
 
 return {
-  s("tests", { t({ "#[cfg(test)]", "mod tests {", "    " }), i(0), t({ "", "}" }) }),
+  s(
+    "tm",
+    fmt(
+      [[
+#[cfg(test)]
+mod tests {{
+    {}
+}}
+]],
+      {
+        i(0),
+      }
+    )
+  ),
+  s(
+    "t",
+    fmt(
+      [[
+#[test]
+fn {}() {{
+    {}
+}}
+]],
+      {
+        i(1),
+        i(0),
+      }
+    )
+  ),
 }
