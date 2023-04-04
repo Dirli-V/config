0d4909211fede7668aef707431fca8682076a03e
Dirli-V <github@dirli.net>
2023-04-04 15:25:49 +0200

    Add coloring of parans

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,12 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "HiPhish/nvim-ts-rainbow2",
+  },
   opts = {
     ensure_installed = "all",
+    rainbow = {
+      enable = true,
+    },
   },
 }
