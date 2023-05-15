03c1bd39e351b83247c1050793b5268c6faae8f7
Dirli-V <github@dirli.net>
2023-05-15 18:03:12 +0200

    Improve notification rendering

--- nvim/lua/plugins/notify.lua
+++ nvim/lua/plugins/notify.lua
@@ -1,5 +1,6 @@
 return {
   "rcarriga/nvim-notify",
+  event = "VeryLazy",
   keys = {
     {
       "<leader>un",
@@ -17,13 +18,7 @@ return {
     max_width = function()
       return math.floor(vim.o.columns * 0.75)
     end,
+    render = "compact",
+    stages = "static",
   },
-  init = function()
-    vim.api.nvim_create_autocmd("User", {
-      pattern = "VeryLazy",
-      callback = function()
-        vim.notify = require("notify")
-      end,
-    })
-  end,
 }
