8212948ff7ca39a6419facc81eb400eff2f1d2c5
Dirli-V <github@dirli.net>
2023-05-17 08:04:57 +0200

    Add additional keymaps for harpoon

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -4,12 +4,89 @@ return {
     "nvim-lua/plenary.nvim",
   },
   keys = {
-    { "<leader>m", function() require("harpoon.mark").add_file() end, desc = "Harpoon file" },
-    { "<leader>h", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Harpoon toggle menu" },
-    { "<leader>1", function() require("harpoon.ui").nav_file(1) end, desc = "Nav to file 1" },
-    { "<leader>2", function() require("harpoon.ui").nav_file(2) end, desc = "Nav to file 2" },
-    { "<leader>3", function() require("harpoon.ui").nav_file(3) end, desc = "Nav to file 3" },
-    { "<leader>4", function() require("harpoon.ui").nav_file(4) end, desc = "Nav to file 4" },
-    { "<leader>5", function() require("harpoon.ui").nav_file(5) end, desc = "Nav to file 5" },
+    {
+      "<leader>m",
+      function()
+        require("harpoon.mark").add_file()
+      end,
+      desc = "Harpoon file",
+    },
+    {
+      "<leader>h",
+      function()
+        require("harpoon.ui").toggle_quick_menu()
+      end,
+      desc = "Harpoon toggle menu",
+    },
+    {
+      "<leader>1",
+      function()
+        require("harpoon.ui").nav_file(1)
+      end,
+      desc = "Nav to file 1",
+    },
+    {
+      "<leader>2",
+      function()
+        require("harpoon.ui").nav_file(2)
+      end,
+      desc = "Nav to file 2",
+    },
+    {
+      "<leader>3",
+      function()
+        require("harpoon.ui").nav_file(3)
+      end,
+      desc = "Nav to file 3",
+    },
+    {
+      "<leader>4",
+      function()
+        require("harpoon.ui").nav_file(4)
+      end,
+      desc = "Nav to file 4",
+    },
+    {
+      "<leader>5",
+      function()
+        require("harpoon.ui").nav_file(5)
+      end,
+      desc = "Nav to file 5",
+    },
+    {
+      "<a-a>",
+      function()
+        require("harpoon.ui").nav_file(1)
+      end,
+      desc = "Nav to file 1",
+    },
+    {
+      "<a-s>",
+      function()
+        require("harpoon.ui").nav_file(2)
+      end,
+      desc = "Nav to file 2",
+    },
+    {
+      "<a-d>",
+      function()
+        require("harpoon.ui").nav_file(3)
+      end,
+      desc = "Nav to file 3",
+    },
+    {
+      "<a-f>",
+      function()
+        require("harpoon.ui").nav_file(4)
+      end,
+      desc = "Nav to file 4",
+    },
+    {
+      "<a-g>",
+      function()
+        require("harpoon.ui").nav_file(5)
+      end,
+      desc = "Nav to file 5",
+    },
   },
 }
