9dc6765e9bf96b612654549a73e47076b6ed37ce
Dirli-V <github@dirli.net>
2023-06-22 16:09:23 +0200

    Add catppuccin plugin integrations

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -3,7 +3,23 @@ return {
   name = "catppuccin",
   lazy = false,
   priority = 1000,
-  config = function()
+  opts = {
+    integrations = {
+      illuminate = true,
+      harpoon = true,
+      leap = true,
+      mini = true,
+      neotree = true,
+      neotest = true,
+      noice = true,
+      notify = true,
+      treesitter_context = true,
+      lsp_trouble = true,
+      which_key = true,
+    },
+  },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
     vim.cmd([[colorscheme catppuccin-mocha]])
   end,
 }
