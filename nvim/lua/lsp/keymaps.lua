6a7893a6316ed2d5ecae3305da8e38fa4da3b730
Dirli-V <github@dirli.net>
2023-06-23 07:58:13 +0200

    Remove has condition from lsp keymaps

--- nvim/lua/lsp/keymaps.lua
+++ nvim/lua/lsp/keymaps.lua
@@ -14,24 +14,24 @@ end
 
 LspKeys = {
   { "gl", vim.diagnostic.open_float, desc = "Line Diagnostics" },
-  { "gd", "<cmd>Telescope lsp_definitions<cr>", desc = "Goto Definition", has = "definition" },
+  { "gd", "<cmd>Telescope lsp_definitions<cr>", desc = "Goto Definition" },
   { "gr", "<cmd>Telescope lsp_references<cr>", desc = "References" },
   { "gD", vim.lsp.buf.declaration, desc = "Goto Declaration" },
   { "gI", "<cmd>Telescope lsp_implementations<cr>", desc = "Goto Implementation" },
   { "gy", "<cmd>Telescope lsp_type_definitions<cr>", desc = "Goto Type Definition" },
   { "K", vim.lsp.buf.hover, desc = "Hover" },
-  { "gK", vim.lsp.buf.signature_help, desc = "Signature Help", has = "signatureHelp" },
-  { "<c-k>", vim.lsp.buf.signature_help, mode = "i", desc = "Signature Help", has = "signatureHelp" },
+  { "gK", vim.lsp.buf.signature_help, desc = "Signature Help" },
+  { "<c-k>", vim.lsp.buf.signature_help, mode = "i", desc = "Signature Help" },
   { "<leader>j", M.diagnostic_goto(true), desc = "Next Diagnostic" },
   { "<leader>k", M.diagnostic_goto(false), desc = "Prev Diagnostic" },
   { "]e", M.diagnostic_goto(true, "ERROR"), desc = "Next Error" },
   { "[e", M.diagnostic_goto(false, "ERROR"), desc = "Prev Error" },
   { "]w", M.diagnostic_goto(true, "WARN"), desc = "Next Warning" },
   { "[w", M.diagnostic_goto(false, "WARN"), desc = "Prev Warning" },
-  { "<a-l>", format, desc = "Format Document", has = "documentFormatting" },
-  { "<a-l>", format, desc = "Format Range", mode = "v", has = "documentRangeFormatting" },
-  { "<leader>cr", vim.lsp.buf.rename, desc = "Rename", has = "rename" },
-  { "<F2>", vim.lsp.buf.rename, desc = "Rename", has = "rename" },
+  { "<a-l>", format, desc = "Format Document" },
+  { "<a-l>", format, desc = "Format Range", mode = "v" },
+  { "<leader>cr", vim.lsp.buf.rename, desc = "Rename" },
+  { "<F2>", vim.lsp.buf.rename, desc = "Rename" },
   {
     "<c-.>",
     function()
@@ -43,19 +43,15 @@ LspKeys = {
   { "<leader>c.", require("helpers").list_code_action_kinds, desc = "List code action kinds" },
 }
 
-function M.on_attach(client, buffer)
+function M.on_attach(_, buffer)
   local Keys = require("lazy.core.handler.keys")
 
   for _, keys in pairs(LspKeys) do
     local parsedKeys = Keys.parse(keys)
-    ---@diagnostic disable-next-line: undefined-field
-    if not parsedKeys.has or client.server_capabilities[parsedKeys.has .. "Provider"] then
-      local opts = Keys.opts(parsedKeys)
-      opts.has = nil
-      opts.silent = opts.silent ~= false
-      opts.buffer = buffer
-      vim.keymap.set(parsedKeys.mode or "n", parsedKeys[1], parsedKeys[2], opts)
-    end
+    local opts = Keys.opts(parsedKeys)
+    opts.silent = opts.silent ~= false
+    opts.buffer = buffer
+    vim.keymap.set(parsedKeys.mode or "n", parsedKeys[1], parsedKeys[2], opts)
   end
 end
 
