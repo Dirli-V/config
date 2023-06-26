d46f232846faf3b75c89d2263f45892763ca0539
Dirli-V <github@dirli.net>
2023-06-26 21:01:22 +0200

    Disable GC on desktop

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -18,11 +18,11 @@
       experimental-features = ["nix-command" "flakes"];
       auto-optimise-store = true;
     };
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 30d";
-    };
+    # gc = {
+    #   automatic = true;
+    #   dates = "weekly";
+    #   options = "--delete-older-than 30d";
+    # };
   };
 
   networking = {
