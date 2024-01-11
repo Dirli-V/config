bf1d3f1421c49f6516448a30d2733c55f74c6ab6
Dirli-V <github@dirli.net>
2024-01-11 21:41:50 +0100

    Don't copy nushell config folder until bugs are fixed

--- nixos/nushell.nix
+++ nixos/nushell.nix
@@ -1,5 +1,8 @@
 {pkgs, ...}: {
-  xdg.configFile.nushell.source = ../nushell;
+  # Wait for fox of either
+  #  - https://github.com/nushell/nushell/issues/10826
+  #  - https://github.com/nushell/nushell/issues/10963
+  # xdg.configFile.nushell.source = ../nushell;
   home.packages = [
     pkgs.nushell
     pkgs.zoxide
