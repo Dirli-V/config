46a71b1e62044f94dcd0f86bee895a6c76d489b4
Dirli-V <github@dirli.net>
2024-01-11 23:09:29 +0100

    Add atuin to nushell and link nushell config

--- nixos/nushell.nix
+++ nixos/nushell.nix
@@ -1,10 +1,31 @@
-{pkgs, ...}: {
-  # Wait for fox of either
-  #  - https://github.com/nushell/nushell/issues/10826
-  #  - https://github.com/nushell/nushell/issues/10963
-  # xdg.configFile.nushell.source = ../nushell;
+{
+  pkgs,
+  config,
+  ...
+}: {
+  xdg.configFile = {
+    # this can probably be done better, but the history file makes it awkward
+    "nushell/history.txt".source = config.lib.file.mkOutOfStoreSymlink "/home/dirli/.local/share/nushell/history.txt";
+    "nushell/atuin.nu".source = ../nushell/atuin.nu;
+    "nushell/bluetooth.nu".source = ../nushell/bluetooth.nu;
+    "nushell/cargo.nu".source = ../nushell/cargo.nu;
+    "nushell/cerebro.nu".source = ../nushell/cerebro.nu;
+    "nushell/config.nu".source = ../nushell/config.nu;
+    "nushell/env.nu".source = ../nushell/env.nu;
+    "nushell/git.nu".source = ../nushell/git.nu;
+    "nushell/killall.nu".source = ../nushell/killall.nu;
+    "nushell/nix.nu".source = ../nushell/nix.nu;
+    "nushell/paru.nu".source = ../nushell/paru.nu;
+    "nushell/systemd.nu".source = ../nushell/systemd.nu;
+    "nushell/venv.nu".source = ../nushell/venv.nu;
+    "nushell/xplr.nu".source = ../nushell/xplr.nu;
+    "nushell/zellij.nu".source = ../nushell/zellij.nu;
+    "nushell/zoxide.nu".source = ../nushell/zoxide.nu;
+    atuin.source = ../atuin;
+  };
   home.packages = [
     pkgs.nushell
     pkgs.zoxide
+    pkgs.atuin
   ];
 }
