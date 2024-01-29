3395c83bfa70e529644d1dde2bc4e858835421b2
Dirli-V <github@dirli.net>
2024-01-29 14:27:40 +0100

    Add python to nvim for ghost

--- nixos/neovim.nix
+++ nixos/neovim.nix
@@ -1,6 +1,13 @@
 {pkgs, ...}: {
   xdg.configFile.nvim.source = ../nvim;
-  home.packages = [pkgs.ripgrep];
+  home.packages = with pkgs; [
+    ripgrep
+    # python needed for ghost
+    python3
+    python311Packages.pynvim
+    python311Packages.requests
+    python311Packages.simple-websocket-server
+  ];
   programs = {
     neovim = {
       enable = true;
