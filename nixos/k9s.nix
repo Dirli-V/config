e6c9d312010e94d11a43808ebe9c5ea154f0f613
Dirli-V <github@dirli.net>
2024-02-16 13:14:44 +0100

    Add proper k9s config

--- nixos/k9s.nix
+++ nixos/k9s.nix
@@ -1,4 +1,18 @@
-{pkgs, ...}: {
-  xdg.configFile.k9s.source = ../k9s;
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: let
+  configFiles = lib.attrsets.concatMapAttrs (filename: _: {
+    "k9s/${filename}".source = ../k9s/${filename};
+  }) (builtins.readDir ../k9s);
+in {
+  xdg.configFile =
+    configFiles
+    // {
+      "k9s/config.yml".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.local/share/k9s/config.yml";
+    };
+
   home.packages = [pkgs.k9s];
 }
