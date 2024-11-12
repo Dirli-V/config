f90e588f644d33d6b7dc6c03d77e90d6f4fec72d
Dirli-V <github@dirli.net>
2024-11-12 22:58:39 +0100

    Move nixos config to personal config

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,26 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    sops = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    stylix = {
-      url = "github:danth/stylix";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
-    };
-    wired-notify = {
-      url = "github:Toqozz/wired-notify";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     scape = {
       url = "github:scape-wm/scape/323397065beb2cdc3db765799c1e33ec7fd2d1df";
-      inputs.nixpkgs.follows = "nixpkgs";
+      # Use the nixpkgs from scape to ensure that the cache has the needed files
+      # inputs.nixpkgs.follows = "nixpkgs";
     };
     ags = {
       url = "github:Aylur/ags";
@@ -30,24 +14,7 @@
     };
   };
 
-  outputs = {nixpkgs, ...} @ inputs: {
-    nixosConfigurations = {
-      dirli-nixos = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        modules = [./desktop.nix];
-      };
-      dirli-surface = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        modules = [./surface.nix];
-      };
-      dirli-laptop = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        modules = [./laptop.nix];
-      };
-    };
-    nixosModules = {
-      default = import ./nixos-default.nix inputs;
-    };
+  outputs = inputs: {
     homeManagerModules = {
       default = import ./hm-default.nix inputs;
     };
