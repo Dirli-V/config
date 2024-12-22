98de611b50d26a83a42014692f484a8071b741cb
Dirli-V <github@dirli.net>
2024-12-23 00:51:33 +0100

    Pass in correct arguments

--- nixos/astal.nix
+++ nixos/astal.nix
@@ -6,6 +6,6 @@ inputs: {
   options.shared-config.astal.enable = lib.mkEnableOption "Enable shared astal config";
 
   config = lib.mkIf config.shared-config.astal.enable {
-    home.packages = [import ../astal/package.nix inputs "x86_64-linux"];
+    home.packages = [(import ../astal/package.nix inputs "x86_64-linux")];
   };
 }
