0eaaab055770d12d3b5aa1b0c6204f2614c93664
Dirli-V <github@dirli.net>
2024-12-23 00:39:50 +0100

    Hard-code system to be linux

--- nixos/astal.nix
+++ nixos/astal.nix
@@ -1,12 +1,11 @@
 inputs: {
   lib,
   config,
-  system,
   ...
 }: {
   options.shared-config.astal.enable = lib.mkEnableOption "Enable shared astal config";
 
   config = lib.mkIf config.shared-config.astal.enable {
-    home.packages = [import ../astal/package.nix inputs system];
+    home.packages = [import ../astal/package.nix inputs "x86_64-linux"];
   };
 }
