15d524042c350e7e78a47f8febc0df1173e3c200
Dirli-V <github@dirli.net>
2025-09-01 12:59:04 +0200

    Enable all other lsp configs

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -8,83 +8,6 @@ function M.diagnostic_goto(next, severity)
   end
 end
 
-local function cursor_not_on_result(result)
-  local target_uri = result.targetUri or result.uri
-  local target_range = result.targetRange or result.range
-
-  local target_bufnr = vim.uri_to_bufnr(target_uri)
-  local target_row_start = target_range.start.line + 1
-  local target_row_end = target_range["end"].line + 1
-  local target_col_start = target_range.start.character + 1
-  local target_col_end = target_range["end"].character + 1
-
-  local current_bufnr = vim.fn.bufnr()
-  local current_range = vim.api.nvim_win_get_cursor(0)
-  local current_row = current_range[1]
-  local current_col = current_range[2] + 1
-
-  return target_bufnr ~= current_bufnr
-    or current_row < target_row_start
-    or current_row > target_row_end
-    or (current_row == target_row_start and current_col < target_col_start)
-    or (current_row == target_row_end and current_col > target_col_end)
-end
-
-local function definition_or_reference()
-  local conf = require("telescope.config").values
-  local finders = require("telescope.finders")
-  local make_entry = require("telescope.make_entry")
-  local pickers = require("telescope.pickers")
-
-  local params = vim.lsp.util.make_position_params()
-  vim.lsp.buf_request(0, "textDocument/definition", params, function(err, result, ctx, _)
-    if err then
-      vim.api.nvim_err_writeln("Error requesting definitions: " .. err.message)
-      return
-    end
-    local flattened_results = {}
-    if result then
-      -- textDocument/definition can return Location or Location[]
-      if not vim.islist(result) then
-        flattened_results = { result }
-      end
-
-      vim.list_extend(flattened_results, result)
-    end
-
-    local offset_encoding = vim.lsp.get_client_by_id(ctx.client_id).offset_encoding
-
-    if #flattened_results == 0 then
-      return
-    end
-
-    if #flattened_results == 1 then
-      if cursor_not_on_result(flattened_results[1]) then
-        vim.lsp.util.jump_to_location(flattened_results[1], offset_encoding, true)
-        return
-      end
-
-      vim.cmd("Telescope lsp_references")
-      return
-    end
-
-    local locations = vim.lsp.util.locations_to_items(flattened_results, offset_encoding)
-    pickers
-      .new({}, {
-        prompt_title = "LSP Definitions",
-        finder = finders.new_table({
-          results = locations,
-          entry_maker = make_entry.gen_from_quickfix({}),
-        }),
-        previewer = conf.qflist_previewer({}),
-        sorter = conf.generic_sorter({}),
-        push_cursor_on_edit = true,
-        push_tagstack_on_edit = true,
-      })
-      :find()
-  end)
-end
-
 local lsp_keys = {
   { "gl", vim.diagnostic.open_float, desc = "Line Diagnostics" },
   { "gd", Snacks.picker.lsp_definitions, desc = "Goto Definition" },
@@ -172,8 +95,17 @@ vim.lsp.config("*", {
 })
 
 vim.lsp.enable({
+  "bashls",
+  "gopls",
+  "intelephense",
+  "jdtls",
+  "jsonls",
   "luals",
   "nixd",
+  "pyright",
+  "taplo",
+  "typos",
+  "yamlls",
 })
 
 return M
