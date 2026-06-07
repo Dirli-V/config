cbbee4d1cb10c32d857eb68c71b7ed69fb0988a5
Dirli-V <github@dirli.net>
2026-06-07 13:27:50 +0200

    Use wezterm from inputs if available

--- nixos/wezterm.nix
+++ nixos/wezterm.nix
@@ -2,15 +2,22 @@
   lib,
   pkgs,
   config,
+  inputs,
   ...
-}: {
+}: let
+  # use input wezterm if available
+  wezterm =
+    if builtins.hasAttr "wezterm" inputs
+    then inputs.wezterm.packages.${pkgs.system}.default
+    else pkgs.wezterm;
+in {
   options.shared-config.wezterm.enable = lib.mkEnableOption "Enable shared wezterm config";
 
   config = lib.mkIf config.shared-config.wezterm.enable {
     xdg.configFile.wezterm.source = ../wezterm;
     home.packages =
       if config.targets.genericLinux.nixGL.packages == null
-      then [pkgs.wezterm]
-      else [(config.lib.nixGL.wrap pkgs.wezterm)];
+      then [wezterm]
+      else [(config.lib.nixGL.wrap wezterm)];
   };
 }
